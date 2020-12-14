e4d6d29c157196620db0cc659987848f62cf2067
chrisfin <chrisfinlayson@gmail.com>
2020-12-14 12:54:41 -0800

    Added support for transfer data and application fees

--- app/models/webhooks.rb
+++ app/models/webhooks.rb
@@ -6,10 +6,18 @@ module Webhooks
 			#if existing_payment_intent.nil?
 				new_payment_intent = Charge.new
 				new_payment_intent.stripe_payment_intent_id = webhook_payment_intent.id
-				new_payment_intent.amount = webhook_payment_intent.amount
 				new_payment_intent.account_id = Account.find_by(stripe_id: webhook_payment_intent.transfer_data.destination).id
 				new_payment_intent.application_fee_amount = webhook_payment_intent.application_fee_amount
-				#new_payment_intent.transfer_data_amount = webhook_payment_intent.charges.data[:transfer_data][:amount]
+				
+				# Determine if Connect platform impelmented transfer data or application fee
+				if (defined? webhook_payment_intent.amount)
+					new_payment_intent.amount = webhook_payment_intent.amount
+				end
+				
+				if (defined? webhook_payment_intent.transfer_data.amount)
+					new_payment_intent.transfer_data_amount = webhook_payment_intent.transfer_data.amount
+				end
+
 				new_payment_intent.save
 			#else
 			#end
